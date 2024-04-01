c1d0618ddad9d03634c6d3b08e053ff9bd1595f3
Yasmin <yasminfayed00@gmail.com>
2024-04-02 01:10:04 +0200

    old app

--- gpio_led_sw_rcc_trial/src/APP/main.c
+++ gpio_led_sw_rcc_trial/src/APP/main.c
@@ -5,14 +5,15 @@
 int main(){
 
     rcc_controlAHB1Peripheral(AHB1_PERIPH_GPIOA,PERIPH_STATE_ENABLED);
-
+    rcc_controlAHB1Peripheral(AHB1_PERIPH_GPIOB,PERIPH_STATE_ENABLED);
+    rcc_controlAHB1Peripheral(AHB1_PERIPH_GPIOC,PERIPH_STATE_ENABLED);
+    rcc_controlAHB1Peripheral(AHB1_PERIPH_GPIOD,PERIPH_STATE_ENABLED);
+    
     led_init();
     sw_init();
-
+    uint8_t sw_state;
     while(1){
 
-        
-        uint8_t sw_state;
         sw_getState(sw_red,&sw_state);
         if(sw_state == SW_STATE_PRESSED){
             led_setState(led_red,LED_STATE_ON);
