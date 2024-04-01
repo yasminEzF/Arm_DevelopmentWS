d55e09a27ff4972ecbcc7029c99894b300b86ecf
Yasmin <yasminfayed00@gmail.com>
2024-04-02 01:03:25 +0200

    lcd HAL module added

--- development/inc/HAL/LCD/lcd.h
+++ development/inc/HAL/LCD/lcd.h
@@ -1,29 +1,47 @@
-#ifndef LED_CFG_H_
-#define LED_CFG_H_
+#ifndef LCD_H_
+#define LCD_H_
 
 /*****************************************************************************/
 /*                              Includes                                     */
 /*****************************************************************************/
-
+#include "HAL/LCD/lcd_cfg.h"
 
 /*****************************************************************************/
 /*                              Defines                                      */
 /*****************************************************************************/
 
 
+
 /*****************************************************************************/
 /*                              Types                                        */
 /*****************************************************************************/
 typedef enum {
-    led_red,
-    led_yellow,
-    led_green,
-    _ledsNum
-}leds_t;
+    lcdState_off,
+    lcdState_init,
+    lcdState_operational
+}lcdState_t;
 
+typedef enum {
+    lcdReqState_noReq,
+    lcdReqState_inProgress,
+    lcdReqState_done
+}lcdReqState_t;
+
+typedef void (*callback_t)(void);
 
 /*****************************************************************************/
 /*                          Function Prototypes                              */
 /*****************************************************************************/
+void lcd_initAsync();
 
-#endif /* LED_CFG_H_ */
+lcdState_t lcd_getLCDState();
+lcdReqState_t lcd_getReqState();
+
+void lcd_writeStringXYAsync(uint8_t* dataStringPtr, uint8_t len, uint8_t x, uint8_t y, callback_t callback_writeXY);
+void lcd_writeStringAsync(uint8_t* dataStringPtr, uint8_t len, callback_t callback_write);
+
+void lcd_clearScreenAsync(callback_t callback_clear);
+void lcd_setCursorAsync(uint8_t x, uint8_t y, callback_t callback_cursor);
+
+
+#endif /* LCD_H_ */
